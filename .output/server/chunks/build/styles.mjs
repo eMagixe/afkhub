028756c0f9594218cae7aea8a29f8b7e1ad5202c
magixe <emax.mails@gmail.com>
2025-07-11 05:23:23 +0500

    Init

---
+++ .output/server/chunks/build/styles.mjs
@@ -0,0 +1,13 @@
+const interopDefault = r => r.default || r || [];
+const styles = {
+  "node_modules/nuxt/dist/app/entry.js": () => import('./entry-styles.Du_d4XfN.mjs').then(interopDefault),
+  "node_modules/nuxt/dist/app/components/error-404.vue": () => import('./error-404-styles.CKJK0xyQ.mjs').then(interopDefault),
+  "node_modules/nuxt/dist/app/components/error-500.vue": () => import('./error-500-styles.DH4SuYyS.mjs').then(interopDefault),
+  "node_modules/nuxt/dist/app/components/welcome.vue": () => import('./welcome-styles.BlKp-ZJO.mjs').then(interopDefault),
+  "node_modules/nuxt/dist/app/components/error-404.vue?vue&type=style&index=0&scoped=06403dcb&lang.css": () => import('./error-404-styles.CFLMZFZX.mjs').then(interopDefault),
+  "node_modules/nuxt/dist/app/components/error-500.vue?vue&type=style&index=0&scoped=4b6f0a29&lang.css": () => import('./error-500-styles.D5zYv3n5.mjs').then(interopDefault),
+  "node_modules/nuxt/dist/app/components/welcome.vue?vue&type=style&index=0&scoped=1e4a8e77&lang.css": () => import('./welcome-styles.CoMkLOSs.mjs').then(interopDefault)
+};
+
+export { styles as default };
+//# sourceMappingURL=styles.mjs.map
