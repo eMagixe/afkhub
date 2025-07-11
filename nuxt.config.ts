f6aad306379270dacac70ccee04d5b0b629495fb
magixe <emax.mails@gmail.com>
2025-07-11 06:10:26 +0500

    Ref host for vite config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui']
+  modules: ['@nuxt/ui'],
+  vite: {
+    server: {
+      allowedHosts: ['afkhub.ru']
+    }
+  }
 })
